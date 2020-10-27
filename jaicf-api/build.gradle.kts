b9e630c95e9d383c2e9bf99ea090dc95940bcb42
Dmitriy Chechyotkin <che@just-ai.com>
2020-10-27 17:34:42 +0300

    Feature/upgrade jaicf version (#82)
    * JAICF dialog API implementation
    
    * JAICF version upgrade

--- jaicf-api/build.gradle.kts
+++ jaicf-api/build.gradle.kts
@@ -21,6 +21,6 @@ dependencies {
     implementation(Library.Android.appCompat)
     implementation(Library.Kotlin.coroutines)
 
-    api("com.justai.jaicf:core:0.7.0")
-    api("com.justai.jaicf:aimybox:0.7.0")
+    api("com.justai.jaicf:core:0.8.0")
+    api("com.justai.jaicf:aimybox:0.8.0")
 }
