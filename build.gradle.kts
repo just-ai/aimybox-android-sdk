efede5bd98aa7e06883eca011c7ddc0ce6c21b94
bgubanov <b.gubanov@list.ru>
2021-11-01 18:25:45 +0300

    0.17.0 Release

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ buildscript {
     }
 }
 
-val versionProject = "0.16.9.8-SNAPSHOT"
+val versionProject = "0.17.0"
 configureRootProject {
     kotlinVersion = Version.kotlin
     version = versionProject
