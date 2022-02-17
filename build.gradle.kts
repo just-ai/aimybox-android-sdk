a74a5a68c8c6a995996358cef7000a65ef2fa8e6
Anton Tinishov <a.tinishov@just-ai.com>
2022-02-17 15:45:14 +0300

    debug github actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ buildscript {
     }
 }
 
-val versionProject = "0.17.4-alpha.2"
+val versionProject = "0.17.4-alpha.2-test-ci"
 configureRootProject {
     kotlinVersion = Version.kotlin
     version = versionProject
