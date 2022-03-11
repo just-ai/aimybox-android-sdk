78138412feddde65f3c615080409a9a6c0a7718d
AlexeyS32 <sakovikh@gmail.com>
2022-03-11 22:35:37 +0300

    MOB-126 AudioSynthesize updated

--- core/src/main/java/com/justai/aimybox/media/AudioSynthesizer.kt
+++ core/src/main/java/com/justai/aimybox/media/AudioSynthesizer.kt
@@ -2,16 +2,16 @@ package com.justai.aimybox.media
 
 import android.content.Context
 import android.media.MediaPlayer
-import com.justai.aimybox.extensions.cancelChildrenAndJoin
 import com.justai.aimybox.extensions.className
-import com.justai.aimybox.extensions.contextJob
 import com.justai.aimybox.logging.Logger
 import com.justai.aimybox.model.AudioSpeech
 import com.justai.aimybox.texttospeech.TextToSpeech
-import kotlinx.coroutines.*
-import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withContext
 import kotlin.coroutines.resume
-import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 /**
  * Designed for usage inside [TextToSpeech] to play SSML audio and [AudioSpeech].
@@ -22,7 +22,7 @@ class AudioSynthesizer(private val context: Context) {
 
     private val L = Logger(className)
 
-   // private var mediaPlayer: MediaPlayer? = null
+    // private var mediaPlayer: MediaPlayer? = null
 
     private var mediaPlayer = MediaPlayer()
 
@@ -39,47 +39,54 @@ class AudioSynthesizer(private val context: Context) {
         launchPlayer(source)
     }
 
-    suspend fun cancel() {
-        mediaPlayer?.stop()
-        mediaPlayer?.reset()
+    fun cancel() {
+        if (mediaPlayer.isPlaying) {
+            mediaPlayer.stop()
+        }
+        mediaPlayer.reset()
     }
 
     fun release() {
-        mediaPlayer?.release()
+        mediaPlayer.release()
     }
 
     private suspend fun launchPlayer(source: AudioSpeech) {
 
-       // mediaPlayer = MediaPlayer()
+        // mediaPlayer = MediaPlayer()
 
         try {
             withContext(Dispatchers.IO) {
-                mediaPlayer?.let {
+                mediaPlayer.let {
                     source.load(context, it)
                 }
 
             }
+            suspendCancellableCoroutine<Unit> { continuation ->
 
-            mediaPlayer?.setOnCompletionListener { player ->
-                player.reset()
-            }
-            mediaPlayer?.setOnPreparedListener { player ->
-                player.start()
-            }
-            mediaPlayer?.setOnErrorListener { player, what, _ ->
-                L.e("MediaPlayer error code $what. Stopping AudioSynthesizer.")
-                player.reset()
-                true
-            }
+                mediaPlayer.setOnCompletionListener { player ->
+                    player.reset()
+                    continuation.resume(Unit)
+                }
 
-            mediaPlayer?.prepareAsync()
+                mediaPlayer.setOnErrorListener { player, what, _ ->
+                    L.e("MediaPlayer error code $what. Stopping AudioSynthesizer.")
+                    player.reset()
+                    continuation.resume(Unit)
+                    true
+                }
+
+                mediaPlayer.setOnPreparedListener { player ->
+                        player.start()
+                }
+
+                mediaPlayer.prepareAsync()
+            }
 
         } catch (e: CancellationException) {
             L.w("AudioSynthesizer is cancelled.")
         } catch (e: Throwable) {
             L.e(e)
-        } finally {
-            mediaPlayer?.reset()
         }
     }
 }
+
